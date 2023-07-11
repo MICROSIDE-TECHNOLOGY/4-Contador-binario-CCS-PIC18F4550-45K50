2cc6b14f74d4e31f1d6c2297d11d128fb83c63a0
vhperaltaj <victor.peralta@microside.com>
2023-07-11 14:09:41 -0600

    Fixed encoding

--- Proyecto/PIC18F45K50/Contador binario.c
+++ Proyecto/PIC18F45K50/Contador binario.c
@@ -5,14 +5,14 @@
 
 /*
 ------------------------------------------------------------------------------------------
- Implementar un contador binario que incrementar· cada vez que se presione un botÛn,
- este ser· configurado como entrada digital. Para visualizar el conteo se utilizan 8 LEDs.
+ Implementar un contador binario que incrementar√° cada vez que se presione un bot√≥n,
+ este ser√° configurado como entrada digital. Para visualizar el conteo se utilizan 8 LEDs.
 ------------------------------------------------------------------------------------------
 */
 
 #include <18F45K50.h>                                   //Incluye el microcontrolador con el que se va a trabajar 
 #use delay(internal=48MHz)                              //Tipo de oscilador y frecuencia dependiendo del microcontrolador 
-#build(reset=0x02000,interrupt=0x02008)                 //Asigna los vectores de reset e interrupciÛn para la versiÛn con bootloader
+#build(reset=0x02000,interrupt=0x02008)                 //Asigna los vectores de reset e interrupci√≥n para la versi√≥n con bootloader
 #org 0x0000,0x1FFF {}                                   //Reserva espacio en memoria para el bootloader
 #define Boton PIN_A2                                    //Asigna el pin A2 al boton BOOT del X-TRAINER
 
@@ -32,7 +32,7 @@ void main(void)
   {
    SisInit ();
 
-   while (1)                                             //Mientras sea verdadero se ejecuta el siguiente cÛdigo
+   while (1)                                             //Mientras sea verdadero se ejecuta el siguiente c√≥digo
 
     {
      if (!input (Boton))                                 //Pregunta el estado del boton
@@ -41,7 +41,7 @@ void main(void)
         cont++;                                          //Se incrementa el contador
         output_b (cont);                                 //Se asigna el valor de cont al puerto B y se despliega el valor
         Delay_ms (5);                                    //Retardo
-        while (!input (Boton));                          //Mientras el botÛn se mantenga igual a cero
+        while (!input (Boton));                          //Mientras el bot√≥n se mantenga igual a cero
         Delay_ms (5);                                    //Retardo
      }
 
